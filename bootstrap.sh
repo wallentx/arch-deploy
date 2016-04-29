3153ce61fd6ff54596afce60f75051a2c62efe2e
wallentx <william.allentx@gmail.com>
2016-04-29 13:32:51 -0500

    adding bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Deploy these dotfiles to your setup.
+
+if [[ $EUID -eq 1000 ]]; then
+#	sudo pacman -S base-devel fakeroot jshon expac git wget&&
+#	wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer &&
+#	mv PKGBUILD\?h\=packer PKGBUILD &&
+#	makepkg &&
+#	sudo pacman -U packer-*.pkg.tar.xz &&
+    for package in $(cat depends.txt); do
+        packer -S $package --noconfirm
+    done
+	echo "All done."
+elif [[ $EUID -eq 0 ]]; then
+	echo Do not run this script as root
+fi
